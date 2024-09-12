1a930659f201b888f12c37a51bc21b0128091410
techiesureshk <techie.sureshk@gmail.com>
2024-09-12 16:34:09 -0500

    permission

--- .github/workflows/ascii-scripts.sh
+++
@@ -1,6 +0,0 @@
-#bin/sh
-sudo apt-get install cowsay -y
-cowsay -f ghostbusters "run for cover,I am a ghostbusters...RAWR" >> ghostbusters.txt
-grep -i " ghostbusters" ghostbusters.txt
-cat ghostbusters.txt
-ls -ltra  
